bf587524c886598750f209e5e5839653b0b3c3f8
Shibo Wang <swang221@my.centennialcollege.ca>
2022-10-24 21:04:02 -0400

    mongodb cloud linked

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/faculty_info",
+  //URI: "mongodb://127.0.0.1/faculty_info",
+  URI:"mongodb+srv://Shibo:18980598216@shibo.tno6qmm.mongodb.net/?retryWrites=true&w=majority",
 };
